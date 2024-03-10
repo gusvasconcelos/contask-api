f1ba6f0f7e0ae92bfed164c586965781bfd9d314
Gustavo Vasconcelos <gusvasconcelos@proton.me>
2024-03-10 19:39:29 -0300

    ✨ feat: crud organizations created

---
+++ src/main/java/dev/vasconcelos/contaskapi/v1/organization/Organization.java
@@ -0,0 +1,30 @@
+package dev.vasconcelos.contaskapi.v1.organization;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "organizations")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Organization {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String cnpj;
+    private String imageUrl;
+}
