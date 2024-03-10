92043ea17075b2ba0e668c4ce70a55f7604fa527
Gustavo Vasconcelos <gusvasconcelos@proton.me>
2024-03-10 16:23:08 -0300

    🎉 init: create project

---
+++ src/main/java/dev/vasconcelos/contaskapi/ContaskApiApplication.java
@@ -0,0 +1,13 @@
+package dev.vasconcelos.contaskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaskApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vasconcelos/contaskapi/ContaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vasconcelos.contaskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
